4ecb620615129a7ee514f2d00f3abe460f83e44c
drmartiner <DrMartiner@GMail.Com>
2014-04-25 11:09:09 +0700

    Fixed url at notice test

--- yandex_money/tests.py
+++ yandex_money/tests.py
@@ -104,7 +104,7 @@ class NoticePaymentTest(WebTest):
         self.payment = Payment(order_amount=87.1)
         self.payment.save()
 
-        self.url = reverse('yandex_money_check')
+        self.url = reverse('yandex_money_notice')
 
     def test_notice(self):
         params = self.params.copy()
